a238ddf877011e29cfa8d851c1d5106ecfb1000f
Prerna <prernakokate29@gmail.com>
2026-03-08 10:49:47 +0530

    Initial React CI/CD demo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: "200px"}}>
+      <h1>Hello Automated CI/CD 🚀</h1>
+      <p>This app was built using a Self-Healing CI/CD Pipeline</p>
+    </div>
+  );
+}
+
+export default App;
